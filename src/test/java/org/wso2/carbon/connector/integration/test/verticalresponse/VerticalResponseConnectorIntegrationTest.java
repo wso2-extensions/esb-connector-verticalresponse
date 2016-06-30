9182a0ad06f265d5291850d3d6fdcbff36d54b90
vives <vsvivevives@gmail.com>
2016-06-30 09:23:51 +0530

    Update VerticalResponseConnectorIntegrationTest.java

--- src/test/java/org/wso2/carbon/connector/integration/test/verticalresponse/VerticalResponseConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/verticalresponse/VerticalResponseConnectorIntegrationTest.java
@@ -57,7 +57,7 @@ public class VerticalResponseConnectorIntegrationTest extends ConnectorIntegrati
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("verticalresponse-connector-1.0.1-SNAPSHOT");
+        init("verticalresponse-connector-1.0.1");
 
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
