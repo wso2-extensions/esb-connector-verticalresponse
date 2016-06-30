347878ee357d7974b1656f44f97c009ea38f5e83
vive <vive@vive.example.com>
2016-06-30 09:37:23 +0530

    Changed connector version

--- src/test/java/org/wso2/carbon/connector/integration/test/verticalresponse/VerticalResponseConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/verticalresponse/VerticalResponseConnectorIntegrationTest.java
@@ -57,7 +57,7 @@ public class VerticalResponseConnectorIntegrationTest extends ConnectorIntegrati
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("verticalresponse-connector-1.0.1");
+        init("verticalresponse-connector-1.0.2-SNAPSHOT");
 
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
